dfcc74875147361cf814b30c1e003b25a435fe2c
Ron Zaretsky <ronz2512@icloud.com>
2023-07-04 15:03:59 +0300

    modified defines.h - added paths to files assets

--- global/defines.h
+++ global/defines.h
@@ -13,5 +13,10 @@
 #define INSTRUCTIONS_COUNT 16
 #define FAILURE 0
 #define SUCCESS 1
+#define AM_FILES_PATH "/assets/am_files/"
+#define AS_FILES_PATH "/assets/as_files/"
+#define OB_FILES_PATH "/assets/ob_files/"
+#define ENT_FILES_PATH "/assets/ent_files/"
+#define EXT_FILES_PATH "/assets/ext_files/"
 
 #endif /* __DEFINES_H__ */
