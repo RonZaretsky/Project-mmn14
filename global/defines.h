bea6edfda6ec38c7f65aae8694822ccd180b1d44
Ron Zaretsky <ronz2512@icloud.com>
2023-07-04 13:06:17 +0300

    added lexer and preprocessor base files added global defines file

---
+++ global/defines.h
@@ -0,0 +1,8 @@
+#define END_OF_STRING '\0'
+#define END_OF_LINE '\n'
+#define AS_FILE_EXTENSION ".as"
+#define AM_FILE_EXTENSION ".am"
+#define OB_FILE_EXTENSION ".ob"
+#define ENT_FILE_EXTENSION ".ent"
+#define EXT_FILE_EXTENSION ".ext"
+#define MAX_LINE_LENGTH 80
---
+++ preprocessor/preprocessor.h
@@ -0,0 +1,9 @@
+#ifndef __PREPROCESSOR_H__
+#define __PREPROCESSOR_H__
+
+
+
+
+
+
+#endif // __PREPROCESSOR_H__
