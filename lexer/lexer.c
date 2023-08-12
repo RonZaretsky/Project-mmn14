9dd8d8872936beaf14c205156f2620eafa729f44
Ron Zaretsky <ronz2512@icloud.com>
2023-08-12 13:37:40 +0300

    finished lexer

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -61,6 +61,7 @@ typedef enum{
 
 
 static void lexer_trie_init();
+static void lexer_trie_deinit();
 static valid_label_err is_valid_label(const char *label);
 static void parse_operation_operands(assembler_ast * ast, char * operands_string, struct op_map * op_map_ptr);
 static void parse_directive(assembler_ast * ast, char * operands_string, struct dir_map * dir_map_ptr);
@@ -70,12 +71,13 @@ static int parse_num(char * num_string, char ** endptr, long * num, long min, lo
 assembler_ast line_to_ast_lexer(char *line){
     assembler_ast ast ={0};
     valid_label_err label_status;
-    struct op_map *op_map_ptr;
-    struct dir_map *dir_map_ptr;
+    struct op_map *op_map_ptr = NULL;
+    struct dir_map *dir_map_ptr = NULL;
     char *aux1, *aux2;
     if(!is_trie_inited){
         lexer_trie_init();
     }
+    line[strcspn(line, "\r\n")] = 0;
     SKIP_SPACES(line);
     aux1 = strchr(line, ':');
     if(aux1){
@@ -114,36 +116,43 @@ assembler_ast line_to_ast_lexer(char *line){
     aux1 = strpbrk(line, SPACE_CHARS);
     if(aux1){
         *aux1 = '\0';
+    
+        aux1++;
+        SKIP_SPACES(aux1);
     }
     if(*line == '.'){
         dir_map_ptr = trie_exists(dir_lookup, line+1);
-    }
-    if(!dir_map_ptr){
-        op_map_ptr = trie_exists(op_lookup, line);
-    }
-    if(!op_map_ptr){
-        sprintf(ast.error_msg, "unknown command '%s'", line);
-        ast.line_type = syntax_error;
-        return ast;
-    }
-
-    line = aux1+1;
-    SKIP_SPACES(line);
-
-    if(op_map_ptr){
-        ast.line_type = op;
-        ast.op_or_dir.op_line.op_type = op_map_ptr->key;
-        parse_operation_operands(&ast, line, op_map_ptr);
-    }
-    if(dir_map_ptr){
+        if(!dir_map_ptr){
+            sprintf(ast.error_msg, "unknown directive '%s'", line+1);
+            ast.line_type = syntax_error;
+            return ast;
+        }
         ast.line_type = dir;
         ast.op_or_dir.dir_line.dir_type = dir_map_ptr->key;
+        parse_directive(&ast, aux1, dir_map_ptr);
     }
+    else{
+        op_map_ptr = trie_exists(op_lookup, line);
+        
+        if(!op_map_ptr){
+            sprintf(ast.error_msg, "unknown command '%s'", line);
+            ast.line_type = syntax_error;
+            return ast;
+        }
+
+        ast.line_type = op;
+        ast.op_or_dir.op_line.op_type = op_map_ptr->key;
+        parse_operation_operands(&ast, aux1, op_map_ptr);
+    }
+    
+    
+    return ast;
 }
 
 static void lexer_trie_init(){
     int i;
     op_lookup = trie();
+    dir_lookup = trie();
     for(i=0;i<16;i++){
         trie_insert(op_lookup, op_map[i].op_name, &op_map[i]);
     }
@@ -173,13 +182,16 @@ static valid_label_err is_valid_label(const char *label){
 }
 
 static void parse_operation_operands(assembler_ast * ast, char * operands_string, struct op_map * op_map_ptr){
-    int operands_count = 0;
     char operand_type;
     char *aux1;
-    char * sep = strchr(operands_string, ',');
+    char * sep = NULL;
+    if(operands_string){
+        sep  = strchr(operands_string, ',');
+    }
+    
     
     if(sep){
-        aux1 - strchr(sep+1, ',');
+        aux1 = strchr(sep+1, ',');
         if(aux1){
             sprintf(ast->error_msg, "too many ','");
             ast->line_type = syntax_error;
@@ -207,6 +219,7 @@ static void parse_operation_operands(assembler_ast * ast, char * operands_string
             ast->line_type = syntax_error;
             return;
         }
+        ast->op_or_dir.op_line.op_operand_option[0] = operand_type == 'I' ? number : operand_type == 'R' ? reg : label; 
         operands_string = sep+1;
         operand_type = parse_operand(operands_string, &ast->op_or_dir.op_line.op_content[1].label_name,&ast->op_or_dir.op_line.op_content[1].const_num,&ast->op_or_dir.op_line.op_content[1].reg_num);
         if(operand_type == 'N'){
@@ -219,11 +232,17 @@ static void parse_operation_operands(assembler_ast * ast, char * operands_string
             ast->line_type = syntax_error;
             return;
         }
+        if(operand_type == 'N'){
+            sprintf(ast->error_msg, "unkown operand '%s' for command '%s'", operands_string, op_map_ptr->op_name);
+            ast->line_type = syntax_error;
+            return;
+        }
         if(strchr(op_map_ptr->dst_options, operand_type) == NULL){
             sprintf(ast->error_msg, "operand '%s' is not supported for command '%s'", operands_string, op_map_ptr->op_name);
             ast->line_type = syntax_error;
             return;
         }
+        ast->op_or_dir.op_line.op_operand_option[1] = operand_type == 'I' ? number : operand_type == 'R' ? reg : label; 
 
     } else {
         if(op_map_ptr->src_options != NULL){
@@ -237,24 +256,29 @@ static void parse_operation_operands(assembler_ast * ast, char * operands_string
             ast->line_type = syntax_error;
             return;
         }
-        if(operand_type == 'E'){
+        if(operand_type == 'E' && op_map_ptr->dst_options != NULL){
             sprintf(ast->error_msg, "command '%s' expects one operand",op_map_ptr->op_name);
             ast->line_type = syntax_error;
             return;
         }
-        if(strchr(op_map_ptr->dst_options, operand_type) == NULL){
+        if(op_map_ptr->dst_options != NULL && strchr(op_map_ptr->dst_options, operand_type) == NULL){
             sprintf(ast->error_msg, "operand '%s' is not supported for command '%s'", operands_string, op_map_ptr->op_name);
             ast->line_type = syntax_error;
             return;
         }
+        ast->op_or_dir.op_line.op_operand_option[1] = operand_type == 'I' ? number : operand_type == 'R' ? reg : label; 
+
     }
 
 }
 
 static char parse_operand(char * operand_string, char ** label, int * const_number, int * register_number){
     char *temp;
-    SKIP_SPACES(operand_string);
     long num;
+    if(operand_string == NULL){
+        return 'E';
+    }
+    SKIP_SPACES(operand_string);
     if(*operand_string == '\0'){
         return 'E';
     }
@@ -277,11 +301,11 @@ static char parse_operand(char * operand_string, char ** label, int * const_numb
         temp = strpbrk(operand_string, SPACE_CHARS);
         if(temp){
             *temp = '\0';
-        }
-        temp++;
-        SKIP_SPACES(temp);
-        if(*temp != '\0'){
-            return 'N';
+            temp++;
+            SKIP_SPACES(temp);
+            if(*temp != '\0'){
+                return 'N';
+            }
         }
         if(is_valid_label(operand_string) != label_ok){
             return 'N';
@@ -304,9 +328,9 @@ static char parse_operand(char * operand_string, char ** label, int * const_numb
 }
 
 static int parse_num(char * num_string, char ** endptr, long * num, long min, long max){
-    errno = 0;
     char * my_end;
     *num = strtol(num_string, &my_end, 10);
+    errno = 0;
     while(isspace(*my_end)){
         my_end++;
     }
@@ -327,7 +351,11 @@ static int parse_num(char * num_string, char ** endptr, long * num, long min, lo
 }
 
 static void parse_directive(assembler_ast * ast, char * operands_string, struct dir_map * dir_map_ptr){
-    char * sep;
+    char * sep1;
+    char * sep2;
+    int num_cnt = 0;
+    int sep_cnt = 0;
+    int curr_num;
     if(dir_map_ptr->key <= dir_entry){
         if(parse_operand(operands_string, &ast->op_or_dir.dir_line.dir_content.label_name, NULL,NULL) != 'L'){
             sprintf(ast->error_msg, "invalid label '%s' for directive '%s'", operands_string, dir_map_ptr->dir_name);
@@ -335,20 +363,75 @@ static void parse_directive(assembler_ast * ast, char * operands_string, struct 
             return;
         }
     }
-    if(dir_map_ptr->key == dir_data){
-        sep = strchr(operands_string, ',');
+    else if(dir_map_ptr->key == dir_data){
+        
         do{
-            if(sep){
-                *sep = '\0';
+            sep1 = strchr(operands_string, ',');
+            if(sep1){
+                *sep1 = '\0';
+                sep_cnt++;
             }
-        }while((sep = strchr(operands_string, ',')) != NULL);
+            switch(parse_operand(operands_string,NULL, &curr_num, NULL)){
+                case 'I':
+                    ast->op_or_dir.dir_line.dir_content.data.data[num_cnt] = curr_num;
+                    num_cnt++;
+                    ast->op_or_dir.dir_line.dir_content.data.data_count = num_cnt;
+                    break;
+                case 'F':
+                    sprintf(ast->error_msg, "directive : %s , error: missing operand", dir_map_ptr->dir_name);
+                    ast->line_type = syntax_error;
+                    return;
+                    break;
+                case 'E':
+                    sprintf(ast->error_msg, "directive : %s , error: why empty? add operands", dir_map_ptr->dir_name);
+                    ast->line_type = syntax_error;
+                    return;
+                    break;
+                default:
+                    sprintf(ast->error_msg, "directive : %s , error: invalid operand", dir_map_ptr->dir_name);
+                    ast->line_type = syntax_error;
+                    return;
+                    break;
+            }
+            if(sep1){
+                operands_string = sep1+1;
+            }else{
+                break;
+            }
+            
+        }while(1);
     }
-    if(dir_map_ptr->key == dir_string){
-    
+    else if(dir_map_ptr->key == dir_string){
+        sep1 = strchr(operands_string, '"');
+        if(!sep1){
+            sprintf(ast->error_msg, "directive : %s , error: missing '\"'", dir_map_ptr->dir_name);
+            ast->line_type = syntax_error;
+            return;
+        }
+        sep1++;
+        sep2 = strchr(sep1, '"');
+        if(!sep2){
+            sprintf(ast->error_msg, "directive : %s , error: missing '\"'", dir_map_ptr->dir_name);
+            ast->line_type = syntax_error;
+            return;
+        }
+        *sep2 = '\0';
+        sep2++;
+        SKIP_SPACES(sep2);
+        if(*sep2 != '\0'){
+            sprintf(ast->error_msg, "directive : %s , error: extra characters after '\"'", dir_map_ptr->dir_name);
+            ast->line_type = syntax_error;
+            return;
+        }
+        ast->op_or_dir.dir_line.dir_content.string = sep1;
     }
 }
 
-
+static void lexer_trie_deinit(){
+    is_trie_inited = 0;
+    trie_destroy(&op_lookup);
+    trie_destroy(&dir_lookup);
+}
 
 
 
